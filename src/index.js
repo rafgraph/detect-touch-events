9afe4e85a05a11a0569cae1de06f1c2b445bf2b1
rafrex <code@rafrex.com>
2017-02-24 11:23:18 -0800

    Update window check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const detectTouchEvents = {
   update() {
-    if (typeof window !== 'undefined') {
+    if (typeof window === 'object') {
       detectTouchEvents.hasApi = 'ontouchstart' in window;
       detectTouchEvents.maxTouchPoints = detectTouchEvents.hasApi ?
         window.navigator && window.navigator.maxTouchPoints :
